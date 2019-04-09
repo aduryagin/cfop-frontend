e3d7ff0f7f479ec57ca3a38ec4ccee172927e8a7
Alexey Duryagin <aiduryagih@gmail.com>
2019-04-09 11:42:33 +0300

    api url

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.prepare()
     // api proxy
 
     server.use(proxyMiddleware('/api', {
-      target: 'http://localhost:8080',
+      target: dev ? 'http://localhost:8080' : 'http://api.cfop.aduryag.in',
       pathRewrite: {
         '^/api': '/query',
       },
