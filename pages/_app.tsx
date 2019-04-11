8f201badde5435f2c21ba3faf556c02fd0b3aa9d
Alexey Duryagin <aiduryagih@gmail.com>
2019-04-11 07:52:53 +0300

    material icons to local

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,31 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
   }
+
+  /* fallback */
+  @font-face {
+    font-family: 'Material Icons';
+    font-style: normal;
+    font-display: auto;
+    font-weight: 400;
+    src: url(https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2) format('woff2'), local('Roboto');
+  }
+
+  .material-icons {
+    font-family: 'Material Icons';
+    font-weight: normal;
+    font-style: normal;
+    font-size: 24px;
+    line-height: 1;
+    letter-spacing: normal;
+    text-transform: none;
+    display: inline-block;
+    white-space: nowrap;
+    word-wrap: normal;
+    direction: ltr;
+    -webkit-font-feature-settings: 'liga';
+    -webkit-font-smoothing: antialiased;
+  }
 `;
 
 type MyAppProps = {
@@ -40,7 +65,6 @@ class MyApp extends App<MyAppProps> {
           <Head>
             <link rel="shortcut icon" href="/static/icons/icon-72x72.png" />
             <link rel="manifest" href="/static/manifest.webmanifest" />
-            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta name="theme-color" content="#ffffff" />
             <meta name="description" content="The CFOP Method (Cross – F2L – OLL – PLL), sometimes known as the Fridrich method, is one of the most commonly used methods in speedsolving a 3×3×3 Rubik's Cube." />
